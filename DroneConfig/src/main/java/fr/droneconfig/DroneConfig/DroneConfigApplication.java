d438c3306b9e0183e46e3f52c2111865e065179d
Le Jeune Matthieu <matthieu.lejeunee@gmail.com>
2023-01-09 23:12:38 +0100

    initial commit

---
+++ DroneConfig/src/main/java/fr/droneconfig/DroneConfig/DroneConfigApplication.java
@@ -0,0 +1,13 @@
+package fr.droneconfig.DroneConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneConfigApplication.class, args);
+	}
+
+}
